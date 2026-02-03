34ed78b0d2d8a91ac9c935c20f788d6c517afba8
jung <guswjddl6176@google.com>
2026-02-03 14:36:39 +0900

    mmca_2

---
+++ js/exh_detail.js
@@ -0,0 +1,34 @@
+$(function(){
+  $('.con_tit i').on('click',function(){
+    history.back();
+  });
+
+  $('#exh_info dd').hide();
+  $('#exh_info').on('click','dt a',function(e){
+    e.preventDefault();
+    $(this).addClass('selected');
+    $(this).parent().next('dd').slideToggle(300);
+  });
+
+  const initTarget = $('.tab li a.tab_on').attr('href');
+  $(`section:not(${initTarget})`).hide();
+  $('.tab').on('click','li a',function(e){
+    e.preventDefault();
+    if($(this).hasClass('tab_on')) return;
+    $('.tab li a').removeClass('tab_on');
+    $(this).addClass('tab_on');
+    $('section').hide();
+    $($(this).attr('href')).fadeIn('fast');
+  });
+
+  $('.reserve_btn').hide();
+  $('body').scroll(function(){
+    let sc_num = $(this).scrollTop();
+    let h_num = $(this).height();
+    if(sc_num>h_num/2){
+      $('.reserve_btn').fadeIn();
+    }else{
+      $('.reserve_btn').hide();
+    }
+  });
+});
--- js/exh_list.js
+++ js/exh_list.js
@@ -7,4 +7,28 @@ $(function(){
       $(this).find('a').stop().fadeOut('fast');
     }
   });
+
+  /* $('#area').on('change',function(){
+    const value = $(this).val();
+    // 페이지 이동
+    if(value){
+      location.href = value;
+    }
+  });
+
+  // select 설정
+  const select = location.pathname.split('/').pop() || 'total_list.html';
+
+  $('#area').find('option').each(function(){
+    if($(this).val() === select){
+      $(this).prop('selected',true);
+    }
+  }); */
+  const select = location.pathname.split('/').pop() || 'total_list.html';
+
+  $('#area').val(select).on('change',function(){
+    if(this.value){
+      location.href = this.value;
+    }
+  });
 });
